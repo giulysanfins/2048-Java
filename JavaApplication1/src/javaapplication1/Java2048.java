9e635cef6449a146a9f2fb32b1571376bc8ddac7
Victor Reis <48132623+Victor-Kings@users.noreply.github.com>
2019-09-21 01:34:07 -0300

    Update Java2048.java

--- JavaApplication1/src/javaapplication1/Java2048.java
+++ JavaApplication1/src/javaapplication1/Java2048.java
@@ -3,30 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package Jogo2048;
+
+import static com.sun.glass.ui.Cursor.setVisible;
+import javax.swing.JFrame;
 
 /**
  *
  * @author 17142837
  */
-public class Java2048 {
+public class Java2048{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         //int aux[][] = null;
          Game g= new Game();
-         Janela janela = new Janela();
-         janela.painel();
-         janela.CapturarTeclado();
-        
-        for(int i=0;i<10;i++){
+         Printar p=new Printar();
+         //Captura tecla=new Captura();
+         //Janela janela = new Janela();
+         //janela.painel();
+         //janela.CapturarTeclado();
+   
+        //tecla.CapturarTeclado();
+         //this.dispose();
+         
+         
+        //for(int i=0;i<10;i++){
             
+            p.setVisible(true);
+            //g.printArray();
+            //g.addNewNumbers();
+            //g.printArray();
+            //g.addNewNumbers();
+            //g.printArray();
+            //g.moveDown();
+            //g.printArray();
+            //aux = g.getGameBoard();          
+            //g.moveLeft();
+             //g.printArray();
+           
+            //aux = g.getGameBoard();
+           
+            //g.moveRight();
+            //aux = g.getGameBoard();
+           
+            //g.moveUp();
+            //aux = g.getGameBoard();
+     
+
+        //}
         
-            g.addNewNumbers();
-            g.printArray();
-        }
         
     }
+
+
+
+
     
 }
