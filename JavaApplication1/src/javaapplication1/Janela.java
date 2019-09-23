ccc5a6d6387a860aa58fab4ecad626c0227acbfd
Victor Reis <48132623+Victor-Kings@users.noreply.github.com>
2019-09-22 23:31:11 -0300

    Update Janela.java

--- JavaApplication1/src/javaapplication1/Janela.java
+++ JavaApplication1/src/javaapplication1/Janela.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package Jogo2048;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -16,9 +16,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;        
 
-public class Janela extends JFrame {
-    ImageIcon img = new ImageIcon(getClass().getResource("Telainicial.png"));
-    JLabel foto = new JLabel(img);
+public class Janela extends JFrame{
+    //ImageIcon img = ;
+    JLabel foto = new JLabel(new ImageIcon(getClass().getResource("Telainicial.png")));
     JButton jogo = new JButton("Jogo");
     JButton creditos = new JButton("Creditos");
     JButton sair = new JButton("Sair");
@@ -30,34 +30,37 @@ public class Janela extends JFrame {
     
     public void painel(){
         
-        setLayout(null); //setando layout como nulo para posicionar botoes
+       setLayout(null); //setando layout como nulo para posicionar botoes
+       //setLayout(new GridLayout(4,1)); //linha x coluna
        foto.setBounds(0, 0, 500, 600);
-        add(foto);
+       add(foto);
                        
        jogo.setContentAreaFilled(false); //fazer botao desaparecer
         
-    
-        
-         jogo.setBounds(0, 235, 425, 80);
+        jogo.setBounds(0, 235, 425, 80);
         add(jogo); //adicionando botoes no painel
-       // add(creditos);
-       // add(sair);
+        add(creditos);
+        add(sair);
         jogo.setFont(fonte);
         creditos.setFont(fonte); //mudando fonte dos botoes
         sair.setFont(fonte);
         
+               //Captura tecla=new Captura();
+        
+       
+        //tecla.CapturarTeclado();
         jogo.addActionListener(new ActionListener(){ //sintaxe para adicionar acao para botao
             public void actionPerformed(ActionEvent e){
+                
                foto.setVisible(false);
-                jogo.setVisible(false); //qnd clicar no jogo desaparece tudo
-                creditos.setVisible(false);
-                sair.setVisible(false);  
+               jogo.setVisible(false); //qnd clicar no jogo desaparece tudo
+               creditos.setVisible(false);
+               sair.setVisible(false);     
                 
             }
-            
         });
         
-        
+
         
        // foto.setBounds(600,600,100,200);
         //add(foto);
@@ -66,39 +69,9 @@ public class Janela extends JFrame {
         setSize(500,600);
         setVisible(true);
         setLocationRelativeTo(this);
+        
+    
        
     }
     
-    public void CapturarTeclado(){
-        addKeyListener(new KeyAdapter(){
-            public void keyPressed(KeyEvent e){
-             int codigo = e.getKeyCode(); //atribuindo teclado
-            //Seta P/ baixo
-        if (e.getKeyCode()==40){
-            System.out.println("Tecla BAIXO"); //API https://docs.oracle.com/javase/1.5.0/docs/api/
-        }
-        //Seta P/ cima        
-        if (e.getKeyCode()==38){
-             System.out.println("Tecla CIMA");
-        }
-        //Seta P/ direita
-        if (e.getKeyCode()==39){
-             System.out.println("Tecla DIREITA");
-        }
-        //Seta P/ esquerda
-        if (e.getKeyCode()==37){
-             System.out.println("Tecla ESQUERDA");
-        }
-             
-//int tecla = KeyEvent.VK_ENTER;
-             
-             //if(codigo == tecla){
-                // System.out.println("Tecla ENTER");
-                 
-            // }
-            }
-            
-        });
-        
-    }
 }
