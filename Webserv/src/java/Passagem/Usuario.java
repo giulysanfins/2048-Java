c0a12e58fe3f274f34ce2b84f2271e86710619e0
Victor Reis <48132623+Victor-Kings@users.noreply.github.com>
2019-10-24 16:11:43 -0300

    Add files via upload

---
+++ Webserv/src/java/Passagem/Usuario.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Passagem;
+
+/**
+ *
+ * @author marcelosiedler
+ */
+public class Usuario {
+    
+    private String login;
+    private String senha;
+    private String email;
+    private String perfil;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+    
+    
+    
+}
---
+++ Webserv/src/java/Passagem/UsuarioDAO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Passagem;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Passagem.Usuario;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author marcelosiedler
+ */
+public class UsuarioDAO {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    public String Var=new String();
+    FileWriter writeFile=null;
+    JSONObject objetoJson=new JSONObject();
+    public UsuarioDAO()
+    {
+    
+    }
+    
+    public boolean inserir(String usuario){
+        objetoJson.put("Movi",usuario);
+        try {
+            writeFile =new FileWriter("build\\classes\\SaveHighScore.txt");
+            writeFile.write(objetoJson.toJSONString());
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        this.Var = usuario;
+        
+        return true;
+    }
+    /*public String visualizar(){
+        Var
+    } */
+    public String getVar() {
+        return Var;
+    }
+
+    
+}
