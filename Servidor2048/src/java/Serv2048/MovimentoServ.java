68d6b372e7705a2b1bbba915543d93666400bce2
Victor Reis <48132623+Victor-Kings@users.noreply.github.com>
2019-11-08 18:36:47 -0300

    Add files via upload
    Servidor do jogo

---
+++ Servidor2048/src/java/Serv2048/MovimentoServ.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serv2048;
+
+import com.google.gson.Gson;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+
+/**
+ * REST Web Service
+ * @author VICTOR
+ */
+@Path("Movimento")
+public class MovimentoServ extends ArqServ {
+
+    @Context
+    private UriInfo context;
+    
+    String Var=new String("teste");
+    public MovimentoServ() {
+    }
+    
+    @GET
+    @Produces("application/Json")
+    public String getJson() {
+        String saida=Read();
+        Gson g= new Gson();
+        String aux=g.toJson(saida);
+        Write("vazio");
+        return aux;
+    }
+    
+    @PUT
+    @Consumes("application/json")
+    @Path("movimento/alterar")
+    public void putInserir(String content){
+        Write(content);
+        Var=content;
+    }
+    
+}
