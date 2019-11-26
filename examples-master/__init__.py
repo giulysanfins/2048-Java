1a4d67ea583e4ace73e28d40d5d5d48255d5e953
giulysanfins <gsanfins@gmail.com>
2019-11-25 23:35:57 -0300

    novo app com tensorflow novo

--- examples-master/__init__.py
+++
@@ -1,6 +0,0 @@
-"""tensorflow_examples is a package for examples of TensorFlow."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
